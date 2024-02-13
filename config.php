fb10609f55ff451220c8bb9fa628a8ddc71ef916
WooYeji <131832439+dubumom@users.noreply.github.com>
2024-02-13 10:23:15 +0900

    DB연결

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$mysqli = new mysqli('localhost', 'simplebbs', '12345', 'simplebbs');
+
+if (mysqli_connect_errno()) {
+  echo "DB연결에 실패했습니다." .mysqlii_connect_errer();
+} else{
+  echo "DB연결 성공";
+}
+?>
